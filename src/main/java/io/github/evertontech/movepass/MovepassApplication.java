225cdd369fcb341876fc5b441c465e74dc0d9768
Everton Santos <everton_projeto@outlook.com>
2024-08-28 14:14:55 -0300

    Initial commit

---
+++ src/main/java/io/github/evertontech/movepass/MovepassApplication.java
@@ -0,0 +1,13 @@
+package io.github.evertontech.movepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovepassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovepassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/evertontech/movepass/MovepassApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.evertontech.movepass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovepassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
