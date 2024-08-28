54e8bda81bdfd5265199f091241eadfebd12609b
Everton Santos <everton_projeto@outlook.com>
2024-08-28 15:30:04 -0300

    criando entidade academia

---
+++ src/main/java/io/github/evertontech/movepass/model/entity/Academia.java
@@ -0,0 +1,24 @@
+package io.github.evertontech.movepass.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Academia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String endereco;
+
+    private Boolean ativo;
+}
