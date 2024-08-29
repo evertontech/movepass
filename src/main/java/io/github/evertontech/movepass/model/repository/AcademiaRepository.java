2f8e215d5654caba1432d9fedbce0dadb7c30c6e
Everton Santos <everton_projeto@outlook.com>
2024-08-29 15:16:22 -0300

    criando repository academia

---
+++ src/main/java/io/github/evertontech/movepass/model/repository/AcademiaRepository.java
@@ -0,0 +1,8 @@
+package io.github.evertontech.movepass.model.repository;
+
+import io.github.evertontech.movepass.model.entity.Academia;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AcademiaRepository extends CrudRepository<Academia, Long> {
+
+}
