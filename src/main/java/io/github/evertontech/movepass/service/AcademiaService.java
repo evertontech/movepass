ae65a03b94995a06ccbc55d5651baa1db425b8b5
Everton Santos <everton_projeto@outlook.com>
2024-08-29 15:16:35 -0300

    criando service academia

---
+++ src/main/java/io/github/evertontech/movepass/service/AcademiaService.java
@@ -0,0 +1,21 @@
+package io.github.evertontech.movepass.service;
+
+import io.github.evertontech.movepass.dto.AcademiaDTO;
+import io.github.evertontech.movepass.model.entity.Academia;
+import io.github.evertontech.movepass.model.repository.AcademiaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AcademiaService {
+
+    @Autowired
+    AcademiaRepository academiaRepository;
+
+    public Academia criar(AcademiaDTO dto) {
+        var entidade = new Academia();
+        entidade.setNome(dto.nome());
+        entidade.setEndereco(dto.endereco());
+        return academiaRepository.save(entidade);
+    }
+}
