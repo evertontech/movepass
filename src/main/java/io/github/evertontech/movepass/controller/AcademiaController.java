f5bf979fb4f6472488ccfd417c1b758c31768f16
Everton Santos <everton_projeto@outlook.com>
2024-08-29 15:16:50 -0300

    criando controller academia

---
+++ src/main/java/io/github/evertontech/movepass/controller/AcademiaController.java
@@ -0,0 +1,27 @@
+package io.github.evertontech.movepass.controller;
+
+import io.github.evertontech.movepass.dto.AcademiaDTO;
+import io.github.evertontech.movepass.model.entity.Academia;
+import io.github.evertontech.movepass.service.AcademiaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/academias")
+@Tag(name = "Academia", description = "Endpoints para Gerenciar Academias")
+public class AcademiaController {
+
+    @Autowired
+    AcademiaService academiaService;
+
+    @PostMapping
+    @Operation(summary = "Cria uma Academia")
+    public Academia criar(@RequestBody AcademiaDTO dto) {
+        return academiaService.criar(dto);
+    }
+}
