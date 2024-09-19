97be36b8a134dae01760adec7842d8b281b4b0d9
Everton Santos <everton_projeto@outlook.com>
2024-09-19 15:44:58 -0300

    novo metodo de interface find by id and ativo true

--- src/main/java/io/github/evertontech/movepass/model/repository/FuncionarioRepository.java
+++ src/main/java/io/github/evertontech/movepass/model/repository/FuncionarioRepository.java
@@ -1,9 +1,11 @@
 package io.github.evertontech.movepass.model.repository;
 
-import io.github.evertontech.movepass.model.entity.Academia;
 import io.github.evertontech.movepass.model.entity.Funcionario;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
 
+    Optional<Funcionario> findByIdAndAtivoTrue();
 }
