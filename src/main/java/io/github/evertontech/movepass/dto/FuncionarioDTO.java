6272fb1c5e619c60d4f23700b926b25625be7398
Everton Santos <everton_projeto@outlook.com>
2024-09-19 15:43:52 -0300

    criando metodo de conversao de dto para entidade

--- src/main/java/io/github/evertontech/movepass/dto/FuncionarioDTO.java
+++ src/main/java/io/github/evertontech/movepass/dto/FuncionarioDTO.java
@@ -1,6 +1,24 @@
 package io.github.evertontech.movepass.dto;
 
+import io.github.evertontech.movepass.model.entity.Funcionario;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FuncionarioDTO {
-    public String nome;
-    public String email;
+    private String nome;
+    private String email;
+
+    public static Funcionario paraEntidade(FuncionarioDTO dto, Long id) {
+        var entidade = new Funcionario();
+        entidade.setNome(dto.getNome());
+        entidade.setEmail(dto.getEmail());
+        entidade.setId(id);
+        return entidade;
+    }
+
+    public static Funcionario paraEntidade(FuncionarioDTO dto) {
+        return paraEntidade(dto, null);
+    }
 }
