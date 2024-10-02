a03f936636b6d0637cdd887c4187b54022d09a52
Everton Santos <everton_projeto@outlook.com>
2024-10-02 15:28:33 -0300

    criando service empresa

---
+++ src/main/java/io/github/evertontech/movepass/service/EmpresaService.java
@@ -0,0 +1,30 @@
+package io.github.evertontech.movepass.service;
+
+import io.github.evertontech.movepass.dto.EmpresaDTO;
+import io.github.evertontech.movepass.model.entity.Empresa;
+import io.github.evertontech.movepass.model.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class EmpresaService {
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    public Empresa criar(EmpresaDTO dto) {
+        var entidade = EmpresaDTO.paraEntidade(dto);
+        return empresaRepository.save(entidade);
+    }
+
+    public Iterable<Empresa> listar() {
+        return empresaRepository.findAllByAtivoTrue();
+    }
+
+    public Optional<Empresa> obterPorId(Long id) {
+        return empresaRepository.findByIdAndAtivoTrue(id);
+    }
+
+}
