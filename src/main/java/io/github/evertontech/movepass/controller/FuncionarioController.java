f50f21fb6be7d7b96c6a3441d80defd600d7e726
Everton Santos <everton_projeto@outlook.com>
2024-10-02 15:27:20 -0300

    reformatando codigo

--- src/main/java/io/github/evertontech/movepass/controller/FuncionarioController.java
+++ src/main/java/io/github/evertontech/movepass/controller/FuncionarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 public class FuncionarioController {
 
     @Autowired
-    FuncionarioService funcionarioService;
+    private FuncionarioService funcionarioService;
 
     @PostMapping
     public Funcionario criarFuncionario(@RequestBody FuncionarioDTO dto) {
--- src/main/java/io/github/evertontech/movepass/dto/FuncionarioDTO.java
+++ src/main/java/io/github/evertontech/movepass/dto/FuncionarioDTO.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class FuncionarioDTO {
+
     private String nome;
     private String email;
 
