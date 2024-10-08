5b5e9fb30be0079a3eb08792431c8463eaed6008
Everton Santos <everton_projeto@outlook.com>
2024-10-08 18:40:13 -0300

    criando assinatura repository

---
+++ src/main/java/io/github/evertontech/movepass/model/repository/AssinaturaRepository.java
@@ -0,0 +1,8 @@
+package io.github.evertontech.movepass.model.repository;
+
+import io.github.evertontech.movepass.model.entity.Assinatura;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AssinaturaRepository extends CrudRepository<Assinatura, Long> {
+
+}
